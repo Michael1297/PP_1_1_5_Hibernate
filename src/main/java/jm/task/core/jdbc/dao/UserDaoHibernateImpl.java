8b92c6dfcc74911bca05fe9652c5e217a186a2de
Michael1297 <38764610+Michael1297@users.noreply.github.com>
2024-01-22 17:30:43 +0300

    remove a transaction from the read method https://stackoverflow.com/questions/11186727/is-it-correct-to-say-that-data-reading-operations-need-not-run-inside-transactio

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -85,21 +85,9 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        Transaction transaction = null;
-        List<User> users;
-
         try (Session session = Util.getSessionFactory().openSession()) {
-            transaction = session.beginTransaction();
-            users = session.createQuery("FROM User", User.class).getResultList();
-            transaction.commit();
-        } catch (Exception e) {
-            if(transaction != null){
-                transaction.rollback();
-            }
-            throw e;
+            return session.createQuery("FROM User", User.class).getResultList();
         }
-
-        return users;
     }
 
     @Override
